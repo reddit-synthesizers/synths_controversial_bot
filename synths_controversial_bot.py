adb10b3cca51d89cf3c41a53633110f9149202c8
SynthBot <synthbot@reddit.com>
2022-03-30 06:02:01 -0700

    Minor cleanup

--- synths_controversial_bot.py
+++ synths_controversial_bot.py
@@ -11,52 +11,54 @@ DEFAULT_SUBREDDIT_NAME = 'synthesizers'
 MIN_COMMENTS_TO_WARN = 10
 MIN_AGE_TO_WARN = 30
 SCORE_THRESHHOLD = 20
+MAX_SUBMISSIONS_TO_PROCESS = 50
 
 
 class SynthsControversialBot:
     def __init__(self, subreddit_name=DEFAULT_SUBREDDIT_NAME, dry_run=False):
         self.dry_run = dry_run
-        self.warning = self.read_text_file('controversial-warning.txt')
-        self.keywords = self.read_keywords_file('controversial-keywords.json')
 
         self.reddit = praw.Reddit('SynthRulesBot')
         subreddit = self.reddit.subreddit(subreddit_name)
 
+        self.warning = self.read_text_file('controversial-warning.txt')
+        self.keywords = self.read_keywords_file('controversial-keywords.json')
+
         # for submission in subreddit.search('title:behringer', sort='new', limit=100):
-        for submission in subreddit.new(limit=50):
+        for submission in subreddit.new(limit=MAX_SUBMISSIONS_TO_PROCESS):
             self.process_submission(submission)
 
     def process_submission(self, submission):
         score = self.get_title_weight(submission)
+        age = self.get_submission_age(submission)
+        downvoted_comments = 0
 
-        if (score > 0 and
-                not submission.approved and
-                not submission.removed and
-                not submission.locked):
+        if (score > 0
+                and age >= MIN_AGE_TO_WARN
+                and submission.num_comments >= MIN_COMMENTS_TO_WARN
+                and not submission.approved
+                and not submission.removed
+                and not submission.locked):
 
-            if (self.get_submission_age(submission) >= MIN_AGE_TO_WARN
-                    and submission.num_comments >= MIN_COMMENTS_TO_WARN):
+            score += self.get_user_reports_count(submission)
 
-                downvoted_comments = 0
-                score += self.get_user_reports_count(submission) 
+            for comment in submission.comments.list():
+                if isinstance(comment, MoreComments):  # ignoring MoreComments (for now)
+                    continue
 
-                for comment in submission.comments.list():
-                    if isinstance(comment, MoreComments):
-                        continue
+                if comment.removed:
+                    score += 1
 
-                    if comment.removed:
-                        score += 1
+                score += comment.controversiality
+                score += self.get_user_reports_count(comment)
 
-                    score += comment.controversiality
-                    score += self.get_user_reports_count(comment) 
+                if comment.score <= 0:
+                    downvoted_comments += 1
 
-                    if comment.score <= 0:
-                        downvoted_comments += 1
+            score += math.ceil(downvoted_comments / submission.num_comments * 100)
 
-                score += math.ceil(downvoted_comments / submission.num_comments * 100)
-
-                if score >= SCORE_THRESHHOLD:
-                    self.warn(submission, score)
+            if score >= SCORE_THRESHHOLD:
+                self.warn(submission, score)
 
     def get_title_weight(self, submission):
         weight = 0
@@ -65,15 +67,14 @@ class SynthsControversialBot:
         for word in self.keywords:
             if title.find(word) >= 0:
                 weight = weight + self.keywords[word]
-                break
 
         return weight
 
     def get_user_reports_count(self, object):
-        count = object.user_reports.__len__()
+        count = len(object.user_reports)
 
         if hasattr(object, 'user_reports_dismissed'):
-            count += object.user_reports_dismissed.__len__()
+            count += len(object.user_reports_dismissed)
 
         return count
 
@@ -92,16 +93,16 @@ class SynthsControversialBot:
         now = datetime.datetime.now()
         created = datetime.datetime.fromtimestamp(submission.created_utc)
         age = now - created
+
         return age.total_seconds() / 60
 
     def was_warned(self, submission):
         warned = False
 
-        comments = submission.comments.list()
-        if comments.__len__() > 0:
-            first_comment = comments.__getitem__(0)
-            warned = (first_comment.distinguished == 'moderator' or
-                      first_comment.author.name == self.reddit.user.me())
+        if len(submission.comments) > 0:
+            first_comment = submission.comments[0]
+            warned = (first_comment.distinguished == 'moderator'  # don't collide with other mods or mod bots
+                      or first_comment.author.name == self.reddit.user.me())
 
         return warned
 
@@ -131,7 +132,7 @@ class SynthsControversialBot:
 
 def lambda_handler(event=None, context=None):
     subreddit_name = os.environ['subreddit_name'] if 'subreddit_name' in os.environ else DEFAULT_SUBREDDIT_NAME
-    dry_run = bool(os.environ['dry_run']) if 'dry_run' in os.environ else False
+    dry_run = os.environ['dry_run'] == 'True' if 'dry_run' in os.environ else False
     SynthsControversialBot(subreddit_name=subreddit_name, dry_run=dry_run)
 
 
