0e43fd8c044a1bd82a493c1fe9a95b9d154dea0b
SynthBot <synthbot@reddit.com>
2022-04-05 06:51:48 -0700

    New experimental build

--- synths_controversial_bot.py
+++ synths_controversial_bot.py
@@ -1,16 +1,18 @@
-import praw
 import datetime
 import json
 import math
 import os
+import praw
 
+from flashtext import KeywordProcessor
 from praw.models import MoreComments
+
 
 DEFAULT_SUBREDDIT_NAME = 'synthesizers'
 
 MIN_COMMENTS_TO_WARN = 10
 MIN_AGE_TO_WARN = 30
-SCORE_THRESHHOLD = 20
+SCORE_THRESHHOLD = 10.0
 MAX_SUBMISSIONS_TO_PROCESS = 50
 
 
@@ -19,64 +21,83 @@ class SynthsControversialBot:
         self.dry_run = dry_run
 
         self.reddit = praw.Reddit('SynthRulesBot')
-        subreddit = self.reddit.subreddit(subreddit_name)
+        self.subreddit = self.reddit.subreddit(subreddit_name)
 
         self.warning = self.read_text_file('controversial-warning.txt')
-        self.keywords = self.read_keywords_file('controversial-keywords.json')
+        self.keywords = self.read_json_file('controversial-keywords.json')
+        self.weights = self.read_json_file('controversial-weights.json')
 
-        # for submission in subreddit.search('title:behringer', sort='new', limit=100):
-        for submission in subreddit.new(limit=MAX_SUBMISSIONS_TO_PROCESS):
+        self.keyword_processor = KeywordProcessor()
+        self.keyword_processor.add_keywords_from_list(list(self.keywords))
+
+    def scan(self):
+        # for submission in self.subreddit.search('title:behringer', sort='new', limit=50):
+        for submission in self.subreddit.new(limit=MAX_SUBMISSIONS_TO_PROCESS):
             self.process_submission(submission)
 
     def process_submission(self, submission):
-        score = self.get_title_weight(submission)
+        score = 0
+        score += self.get_title_score(submission)
+        score += self.get_body_score(submission)
+
         age = self.get_submission_age(submission)
-        downvoted_comments = 0
 
         if (score > 0
                 and age >= MIN_AGE_TO_WARN
                 and submission.num_comments >= MIN_COMMENTS_TO_WARN
+                and not submission.distinguished == 'moderator'
                 and not submission.approved
                 and not submission.removed
                 and not submission.locked):
 
             score += self.get_user_reports_count(submission)
-
-            for comment in submission.comments.list():
-                if isinstance(comment, MoreComments):  # ignoring MoreComments (for now)
-                    continue
-
-                if comment.removed:
-                    score += 1
-
-                score += comment.controversiality
-                score += self.get_user_reports_count(comment)
-
-                if comment.score <= 0:
-                    downvoted_comments += 1
-
-            score += math.ceil(downvoted_comments / submission.num_comments * 100)
+            score += self.get_comments_score(submission)
+            score = round(score, 2)
 
             if score >= SCORE_THRESHHOLD:
                 self.warn(submission, score)
+            elif score >= SCORE_THRESHHOLD / 1.5:
+                self.log('Trending (score=' + str(score) + ')', submission)
 
-    def get_title_weight(self, submission):
-        weight = 0
+    def get_title_score(self, submission):
+        score = 0
 
-        title = submission.title.lower()
-        for word in self.keywords:
-            if title.find(word) >= 0:
-                weight = weight + self.keywords[word]
+        for keyword in self.keyword_processor.extract_keywords(submission.title):
+            score += self.keywords[keyword]
 
-        return weight
+        return score
 
-    def get_user_reports_count(self, object):
-        count = len(object.user_reports)
+    def get_body_score(self, submission):
+        score = 0
 
-        if hasattr(object, 'user_reports_dismissed'):
-            count += len(object.user_reports_dismissed)
+        for keyword in self.keyword_processor.extract_keywords(submission.selftext):
+            score += self.keywords[keyword]
 
-        return count
+        return score
+
+    def get_comments_score(self, submission):
+        score = 0
+        downvoted_comments = 0
+
+        for comment in submission.comments.list():
+            if isinstance(comment, MoreComments):  # ignoring MoreComments (for now)
+                continue
+
+            if comment.removed:
+                score += self.weights['removed']
+
+            score += comment.controversiality * self.weights['controversial']
+            score += self.get_user_reports_count(comment) * self.weights['reported']
+
+            if comment.score <= 0:
+                downvoted_comments += 1
+
+            for keyword in self.keyword_processor.extract_keywords(comment.body):
+                score += self.keywords[keyword]
+
+        score += math.ceil(downvoted_comments / submission.num_comments * self.weights['downvoted'])
+
+        return score
 
     def warn(self, submission, score):
         if not self.was_warned(submission):
@@ -89,13 +110,6 @@ class SynthsControversialBot:
 
             self.log('Warned (score=' + str(score) + ')', submission)
 
-    def get_submission_age(self, submission):
-        now = datetime.datetime.now()
-        created = datetime.datetime.fromtimestamp(submission.created_utc)
-        age = now - created
-
-        return age.total_seconds() / 60
-
     def was_warned(self, submission):
         warned = False
 
@@ -106,34 +120,49 @@ class SynthsControversialBot:
 
         return warned
 
-    def read_text_file(self, filename):
-        text = {}
+    @staticmethod
+    def get_user_reports_count(obj):
+        count = len(obj.user_reports)
 
-        file = open(filename, 'r')
-        text = file.read()
-        file.close()
+        if hasattr(obj, 'user_reports_dismissed'):
+            count += len(obj.user_reports_dismissed)
+
+        return count
+
+    @staticmethod
+    def get_submission_age(submission):
+        now = datetime.datetime.now()
+        created = datetime.datetime.fromtimestamp(submission.created_utc)
+        age = now - created
+
+        return age.total_seconds() / 60
+
+    @staticmethod
+    def read_text_file(filename):
+        with open(filename, encoding='utf-8') as file:
+            text = file.read()
 
         return text
 
-    def read_keywords_file(self, filename):
-        data = {}
-
-        file = open(filename, 'r')
-        data = json.load(file)
-        file.close()
+    @staticmethod
+    def read_json_file(filename):
+        with open(filename, encoding='utf-8') as file:
+            data = json.load(file)
 
         return data
 
     def log(self, message, submission):
-        now = datetime.datetime.now()
+        is_dry_run = '*' if self.dry_run is True else ''
         name = type(self).__name__
-        print(f'[{name}][{now}] {message}: \'{submission.title}\' ({submission.id})')
+        now = datetime.datetime.now()
+        print(f'{is_dry_run}[{name}][{now}] {message}: \'{submission.title}\' ({submission.id})')
 
 
 def lambda_handler(event=None, context=None):
     subreddit_name = os.environ['subreddit_name'] if 'subreddit_name' in os.environ else DEFAULT_SUBREDDIT_NAME
     dry_run = os.environ['dry_run'] == 'True' if 'dry_run' in os.environ else False
-    SynthsControversialBot(subreddit_name=subreddit_name, dry_run=dry_run)
+    bot = SynthsControversialBot(subreddit_name=subreddit_name, dry_run=dry_run)
+    bot.scan()
 
 
 if __name__ == '__main__':
